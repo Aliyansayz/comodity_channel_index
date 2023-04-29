46c73757aeef040279e38c56e11cd9ec35045d99
Aliyan Anwar <56589240+Aliyansayz@users.noreply.github.com>
2023-04-29 15:51:24 +0500

    Update comodity_channel_index.py

--- comodity_channel_index.py
+++ comodity_channel_index.py
@@ -22,6 +22,19 @@ def sma (array, period ):
         sma[i-1] = np.mean(array[i-period:i]) 
   return sma 
 
+
+def ema (self, array, period ):
+    import numpy as np
+
+    ema = np.empty_like(array)
+    ema = np.full( ema.shape , np.nan)
+    ema[0] = np.mean(array[0] , dtype=np.float16)
+    alpha = 2 / (period + 1)
+    # Calculate the EMA for each window of 14 values
+    for i in range(1 , len(array) ):
+          ema[i] = np.array( (array[i] * alpha +  ema[i-1]  * (1-alpha) ) , dtype=np.float16 )
+    
+    return ema 
 def deviation(typical_price , typical_ma ):
      
     return abs(typical_price - typical_ma)
@@ -32,10 +45,10 @@ def cci( period ):
     typical_price = np.mean((np.hstack((high , low , close)).reshape(-1,3) ), axis=1 )
     # mean(typical)
     typical_ma = sma( typical_price , period )
-    typical_ma[:period-1] = (typical_price[:period-1] * 1/period) + typical_price[:period-1]
+    typical_ma[:period-1] = ema( typical_price[:period-1] , period ) 
     dev = deviation( typical_price , typical_ma ) 
-    mean_deviation = sma( dev  , period) 
-    mean_deviation[:period-1] =  (dev[:period-1] * 1/period) + dev[:period-1]
+    mean_deviation = sma( abs(dev)  , period) 
+    mean_deviation[:period-1] =   ema( dev[:period-1]  , period )
 
     cci = ( typical_price - typical_ma ) / ( 0.015 * mean_deviation ) 
     
